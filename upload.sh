effb9e3d4b6a545579721373ea0ee6e47f6a4127
Wolfgang Huber <wolfgang.huber@embl.org>
2023-01-17 15:46:40 +0100

    Updates

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 chmod 775 _site
 find _site -type d -exec chmod 775 {} \;
+find _site -type f -exec chmod 555 {} \;
 rsync -rvlpt _site/ spinoza.embl.de:/g/huber/www-huber/users/whuber/2301-GHGA-best-analysis-practices
 
 #ssh spinoza.embl.de rm -r /g/huber/www-huber/users/whuber/2301-GHGA-best-analysis-practices
